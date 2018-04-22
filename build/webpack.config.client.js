939d6984835cbfde4c9929b7627af54fa804fb89
jingchaoyang <jingchaoyang@autohome.com.cn>
2018-04-22 15:56:13 +0800

    添加文件一个基础模板

---
+++ build/webpack.config.client.js
@@ -0,0 +1,126 @@
+const path = require('path')
+const HTMLPlugin = require('html-webpack-plugin')
+const webpack = require('webpack')
+const ExtractPlugin = require('extract-text-webpack-plugin') // css 分离插件
+const baseConfig = require('./webpack.config.base')
+const merge = require('webpack-merge') // 合并webpack配置
+
+// 区分环境
+const isDev = process.env.NODE_ENV === 'development'
+const devServer = {
+  port: 9000,
+  host: '0.0.0.0',
+  overlay: {
+    errors: true,
+  },
+  hot: true
+};
+const defaultPlugin = [
+  new webpack.DefinePlugin({
+    'process.env': {
+      NODE_ENV: isDev ? '"development"' : '"production"'
+    }
+  }),
+  new HTMLPlugin()
+];
+let config;
+if (isDev) {
+  config = merge(baseConfig, {
+    devtool: '#cheap-module-eval-source-map',
+    devServer,
+    module: {
+      rules: [
+        {
+          test: /\.styl$/,
+          use: [
+            'style-loader',
+            'css-loader',
+            {
+              loader: 'postcss-loader',
+              options: {
+                sourceMap: true,
+              }
+            },
+            'stylus-loader'
+          ]
+        },
+        {
+          test: /\.scss$/,
+          use: [
+            'style-loader',
+            'css-loader',
+            {
+              loader: 'postcss-loader',
+              options: {
+                sourceMap: true,
+              }
+            },
+            'sass-loader'
+          ]
+        }
+      ]
+    },
+    plugins: defaultPlugin.concat([
+      new webpack.HotModuleReplacementPlugin(),
+      new webpack.NoEmitOnErrorsPlugin()
+    ])
+  });
+} else {
+  config = merge(baseConfig, {
+    entry: {
+      app: path.join(__dirname, '../src/index.js'),
+      vendor: ['vue'] // 提取公共代码
+    },
+    output: {
+      filename: '[name].[chunkhash:8].js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.styl/,
+          use: ExtractPlugin.extract({
+            fallback: 'style-loader',
+            use: [
+              'css-loader',
+              {
+                loader: 'postcss-loader',
+                options: {
+                  sourceMap: true,
+                }
+              },
+              'stylus-loader'
+            ]
+          })
+        },
+        {
+          test: /\.scss/,
+          use: ExtractPlugin.extract({
+            fallback: 'style-loader',
+            use: [
+              'css-loader',
+              {
+                loader: 'postcss-loader',
+                options: {
+                  sourceMap: true,
+                }
+              },
+              'sass-loader'
+            ]
+          })
+        }
+      ]
+    },
+    plugins: defaultPlugin.concat([
+      new ExtractPlugin('styles.[contentHash:8].css'),
+      // 抽离公共js
+      new webpack.optimize.CommonsChunkPlugin({
+        name: 'vendor'
+      }),
+      new webpack.optimize.CommonsChunkPlugin({
+        name: 'runtime'
+      })
+    ])
+  });
+}
+
+module.exports = config
---
+++ src/views/layout/footer.jsx
@@ -0,0 +1,16 @@
+import '../../assets/styles/footer.scss'
+
+export default {
+  data() {
+    return {
+      author: 'Jokcy'
+    }
+  },
+  render() {
+    return (
+      <div id="footer">
+        <span>Written by {this.author}</span>
+      </div>
+    )
+  }
+}
